e8e2fc4010345cf6cccd7650ffad6c8efe95c670
malik1243 <154369335+basit1215@users.noreply.github.com>
2024-12-22 17:26:52 +0500

    first commit

---
+++ info.js
@@ -0,0 +1,33 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: 'assets/Song 1.mp3',
+        artist: 'artist 1',
+        cover: 'assets/song1.png'
+    },
+    {
+        name: 'song 2',
+        path: 'assets/Song 2.mp3',
+        artist: 'artist 2',
+        cover: 'assets/song2.png'
+    },
+    {
+        name: 'song 3',
+        path: 'assets/Song 3.mp3',
+        artist: 'artist 3',
+        cover: 'assets/song3.png'
+    },
+    {
+        name: 'song 4',
+        path: 'assets/Song 4.mp3',
+        artist: 'artist 4',
+        cover: 'assets/song4 (2).png'
+    },
+    {
+        name: 'song 5',
+        path: 'assets/Song 5.mp3',
+        artist: 'artist 5',
+        cover: 'assets/song5.png'
+    },
+]
+    
